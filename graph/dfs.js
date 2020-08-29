350906482ae510f6e2d06903cf5d58c825a76c47
Ahm Asaduzzaman <Ahm.Asaduzzaman@libertymutual.com>
2020-08-29 18:03:29 -0400

    dfs

--- graph/dfs.js
+++ graph/dfs.js
@@ -1,77 +1,64 @@
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+   /**
+     * Depth-First graph searching algorithm.
+     * Returns whether there's a path between two nodes in a graph.
+     * Time complexity: O(|V|^2).
+     *
+     * @param {Array} graph Adjacency matrix, which represents the graph.
+     * @param {Number} start Start node.
+     * @param {Number} goal Target node.
+     * @return {Boolean} Returns true if path between two nodes exists.
+     *
+     * const graph = [[1, 1, 0, 0, 1, 0],
+     *              [1, 0, 1, 0, 1, 0],
+     *              [0, 1, 0, 1, 0, 0],
+     *              [0, 0, 1, 0, 1, 1],
+     *              [1, 1, 0, 1, 0, 0],
+     *              [0, 0, 0, 1, 0, 0]];
+     * const pathExists = dfs(graph, 1, 5); // true
+     */
 
-const routes = [
-    ['PHX', 'LAX'],
-    ['PHX', 'JFK'],
-    ['JFK', 'OKC'],
-    ['JFK', 'HEL'],
-    ['JFK', 'LOS'],
-    ['MEX', 'LAX'],
-    ['MEX', 'BKK'],
-    ['MEX', 'LIM'],
-    ['MEX', 'EZE'],
-    ['LIM', 'BKK'],
-];
-
-
-function Graph (uvList) {
-  this.adjacencyList = new Map();
-  this.uvList = uvList
-}
-
-Graph.prototype.addNode = function(vertex) {
-  this.adjacencyList.set(vertex, [])
-}
-
-Graph.prototype.addEdge = function(u, v) {
-  this.adjacencyList.get(u).push(v)
-  // this.adjacencyList.get(v).push(u)
-}
-
-let nVertex = airports.length
-let uvList = routes
-
-let g = new Graph(uvList)
-airports.forEach((v) => {
-  g.addNode(v)
-})
-
-function dfs(start, visited = new Set()) {
-  console.log(start)
-  visited.add(start)
-  const destinations = adjacencyList.get(start)
-  for (const destination of destinations) {
-    if (destination === 'BKK') { 
-      console.log(`DFS found Bangkok`)
-      return
-    }
-    if (!visited.has(destination)) {
-      dfs(destination, visited)
-    }
-  }
-}
-
-
-function bfs(start) {
-  const visited = new Set()
-  const queue = [start]
-  while (queue.length > 0) {
-    const airport = queue.shift()
-    const destinations = adjacencyList.get(airport)
-    for (const destination of destinations) {
-      if (destination === 'BKK')  {
-        console.log(`BFS found Bangkok!`)
-      }
-      if (!visited.has(destination)) {
-        visited.add(destination)
-        queue.push(destination)
+const graph = [
+  [1, 1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0]
+];     
+const dfs = (graph, current, goal) => {
+  let stack = []
+  let visited = []
+  stack.push(current)
+  visited[current] = true
+  while(stack.length) {
+    let node = stack.pop()
+    console.log('node: ', node)
+    if(node === goal)
+      return true
+    for(let i = 0; i < graph[node].length; i++) {
+      console.log(graph[node][i])
+      if (graph[node][i] && !visited[i]) {
+        stack.push(i);
+        visited[i] = true;
       }
     }
   }
+  return false;
 }
 
-bfs('PHX')
-dfs('PHX')
-
-routes.forEach(route => g.addEdge(...route))
-console.log(g.adjacencyList)
+var gfg = new Array(10); 
+for (var i = 0; i < gfg.length; i++) { 
+  gfg[i] = new Array(10); 
+} 
+v = 0
+for (var i = 0; i < 10; i++) { 
+  for (var j = 0; j < 10; j++) { 
+      gfg[i][j] = v++; 
+  } 
+} 
+let s =[[]]
+let a = [0,1]
+let b = [2,3]
+s.push =a
+s.push = b
+console.log(gfg)
