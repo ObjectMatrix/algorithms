6e9e15aeffcb923e94e6c3fbd5257112aa2592a6
Ahm Asaduzzaman <asad.gooogle@gmail.com>
2021-12-29 13:50:54 -0500

    Update sortedToBalancedTree.js

--- graph/sortedToBalancedTree.js
+++ graph/sortedToBalancedTree.js
@@ -45,7 +45,7 @@ class Node
     }
 }
  
-var root = null;
+let root = null;
  
 /* A function that constructs Balanced Binary Search Tree 
  from a sorted array */
@@ -57,13 +57,11 @@ function sortedArrayToBST(arr, start, end)
         return null;
     }
     /* Get the middle element and make it root */
-    var mid = Math.floor((start + end) / 2);
-    var node = new Node(arr[mid]);
-    /* Recursively construct the left subtree and make it
-     left child of root */
+    let mid = Math.floor((start + end) / 2);
+    let node = new Node(arr[mid]);
+    /* Recursively construct the left subtree and make it left child of root */
     node.left = sortedArrayToBST(arr, start, mid - 1);
-    /* Recursively construct the right subtree and make it
-     right child of root */
+    /* Recursively construct the right subtree and make it right child of root */
     node.right = sortedArrayToBST(arr, mid + 1, end);
     return node;
 }
@@ -80,8 +78,8 @@ function preOrder(node)
 }
  
  
-var arr = [1, 2, 3, 4, 5, 6, 7];
-var n = arr.length;
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let n = arr.length;
 root = sortedArrayToBST(arr, 0, n - 1);
 
 preOrder(root);
